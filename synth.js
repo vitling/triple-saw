b575ebb31060aee1e5aa5337c782de41f988385a
David Whiting <davw@apache.org>
2020-04-07 13:08:54 +0200

    Extra visuals

--- synth.js
+++ synth.js
@@ -80,6 +80,79 @@ function WanderingParameter(lowerBound, upperBound, drift, correction) {
     return {moveAndGet: moveAndGet, value: () => value};
 }
 
+function Visualiser() {
+    const crt = createCRT(null, "./filters.svg", 0.4);
+    document.getElementById("wrapper").append(crt.screen);
+    const g = crt.context;
+    const w = crt.canvas.width;
+    const h = crt.canvas.height;
+
+    const fontSize = 20;
+    g.font = fontSize + "px 'Share Tech Mono'";
+
+    function visualisePart(out, color, offsetX, offsetY = fontSize * 2) {
+        const bW = (w/(3 * 24));
+        const bH = fontSize;
+        g.globalAlpha = 0.3;
+        g.fillStyle = color;
+        JSON.stringify(out,null,1).split("\n").forEach((line, i) => g.fillText(line, offsetX, offsetY +i * bH));
+        g.globalAlpha = 1;
+
+        for (let i = 0; i < out.seq.length; i++) {
+            let y = offsetY + (4 + i) * bH;
+            let x = offsetX + out.seq[i] * bW;
+            let isStep = i === out.step;
+            g.fillStyle = isStep ? "white" : color;
+            g.fillRect(x,y,bW,bH);
+            if (isStep) {
+                g.fillRect(offsetX,y + bH/2, 2, 2);
+            }
+        }
+
+        g.globalCompositeOperation = "difference";
+        g.strokeStyle = color;
+        g.lineWidth = 6;
+        g.beginPath();
+        let y = offsetY  + bH * 4 + + h * out.filter/10000;
+        g.moveTo(offsetX, y);
+        g.lineTo(offsetX + bW * 24, y);
+        g.stroke();
+        g.globalCompositeOperation = "source-over";
+    }
+
+    function displayCurrentKey() {
+        g.fillStyle = "white";
+        const key = NOTE_NAMES[baseKey % 12];
+        g.fillText("Key: " + key + " minor", 10, fontSize);
+    }
+
+    function randomRectangles() {
+        g.save();
+        g.globalCompositeOperation = "difference";
+        for (let i = 0; i < 5; i++) {
+            g.fillStyle = choose(["magenta", "cyan", "yellow", "white"]);
+            let x = rndInt(w);
+            let y = rndInt(h);
+            let rw = rndInt(w - x);
+            let rh = rndInt(h - y);
+            g.fillRect(x, y, rw, rh);
+        }
+        g.restore();
+    }
+
+    function visualise(leftOut, bassOut, rightOut) {
+        g.clearRect(0,0,w,h);
+
+        visualisePart(leftOut, "magenta",0);
+        visualisePart(bassOut, "yellow",w/3);
+        visualisePart(rightOut, "cyan",2 * w/3);
+
+        displayCurrentKey();
+    }
+
+    return {visualise, randomRectangles, crtRefresh: () => crt.paintCanvasToScreen()}
+}
+
 function start() {
 
     const audio = new (window.AudioContext || window.webkitAudioContext)();
@@ -144,64 +217,7 @@ function start() {
         return {step}
     }
 
-    const crt = createCRT(null, "./filters.svg", 0.4);
-    document.getElementById("wrapper").append(crt.screen);
-    const g = crt.context;
-    const w = crt.canvas.width;
-    const h = crt.canvas.height;
-
-    const fontSize = 20;
-    g.font = fontSize + "px 'Share Tech Mono'";
-
-    function visualisePart(out, color, offsetX, offsetY = fontSize * 2) {
-        const bW = (w/(3 * 24));
-        const bH = fontSize;
-        g.globalAlpha = 0.3;
-        g.fillStyle = color;
-        JSON.stringify(out,null,1).split("\n").forEach((line, i) => g.fillText(line, offsetX, offsetY +i * bH));
-        g.globalAlpha = 1;
-
-        for (let i = 0; i < out.seq.length; i++) {
-            let y = offsetY + (4 + i) * bH;
-            let x = offsetX + out.seq[i] * bW;
-            let isStep = i === out.step;
-            g.fillStyle = isStep ? "white" : color;
-            g.fillRect(x,y,bW,bH);
-            if (isStep) {
-                g.fillRect(offsetX,y + bH/2, 2, 2);
-            }
-        }
-
-        g.globalCompositeOperation = "difference";
-        g.strokeStyle = color;
-        g.lineWidth = 6;
-        g.beginPath();
-        let y = offsetY  + bH * 4 + + h * out.filter/10000;
-        g.moveTo(offsetX, y);
-        g.lineTo(offsetX + bW * 24, y);
-        g.stroke();
-        g.globalCompositeOperation = "source-over";
-    }
-
-    function displayCurrentKey() {
-        g.fillStyle = "white";
-        const key = NOTE_NAMES[baseKey % 12];
-        g.fillText("Key: " + key + " minor", 10, fontSize);
-    }
-
-    function randomRectangles() {
-        g.save();
-        g.globalCompositeOperation = "difference";
-        for (let i = 0; i < 5; i++) {
-            g.fillStyle = choose(["magenta", "cyan", "yellow", "white"]);
-            let x = rndInt(w);
-            let y = rndInt(h);
-            let rw = rndInt(w - x);
-            let rh = rndInt(h - y);
-            g.fillRect(x, y, rw, rh);
-        }
-        g.restore();
-    }
+    const vis = Visualiser();
 
     const kick = new Kick(audio);
     kick.out.connect(mainOut);
@@ -223,21 +239,15 @@ function start() {
         let rightOut = right.step();
         let bassOut = bass.step();
 
-        g.clearRect(0,0,w,h);
-
-        visualisePart(leftOut, "magenta",0);
-        visualisePart(bassOut, "yellow",w/3);
-        visualisePart(rightOut, "cyan",2 * w/3);
-
-        displayCurrentKey();
+        vis.visualise(leftOut, bassOut, rightOut);
 
         if (mondrianEnabled) {
-            randomRectangles();
+            vis.randomRectangles();
         }
     }
 
     function paint() {
-        crt.paintCanvasToScreen();
+        vis.crtRefresh();
     }
 
 
