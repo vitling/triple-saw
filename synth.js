962143ef27dc4f9db796256160b9d079ae8993cd
David Whiting <davw@apache.org>
2020-04-07 12:35:23 +0200

    Extract devices

--- synth.js
+++ synth.js
@@ -1,4 +1,5 @@
 import { createCRT } from "./crt.js";
+import { MonoSynth, Kick, FeedbackDelay, Panner } from "./devices.js";
 
 const startButton = document.getElementById("startButton");
 
@@ -15,99 +16,6 @@ function start() {
 
     const audio = new (window.AudioContext || window.webkitAudioContext)();
 
-    class MonoSynth {
-        constructor(type="sawtooth") {
-            this.osc = audio.createOscillator();
-            this.amp = audio.createGain();
-            this.filter = audio.createBiquadFilter();
-            this.osc.connect(this.filter);
-            this.filter.connect(this.amp);
-            this.out = this.amp;
-
-            this.osc.type = type;
-            this.osc.frequency.setValueAtTime(110, audio.currentTime);
-            this.amp.gain.setValueAtTime(0.1, audio.currentTime);
-            this.osc.start();
-
-            this.filter.type = "lowpass";
-            this.filter.frequency.setValueAtTime(440, audio.currentTime);
-            this.filter.Q.setValueAtTime(10, audio.currentTime);
-        }
-        connect(target) {
-            this.out.connect(target);
-        }
-        play(pitch, filterFreq, decay = secondsPer16th * 4) {
-            let ctime = audio.currentTime+0.01;
-            const attack = 0.04;
-            const glide = 0.005;
-            const error = Math.random()*0.01 - 0.005;
-
-            this.osc.frequency.cancelScheduledValues(ctime);
-            this.osc.frequency.setTargetAtTime((1 + error) * pitch, ctime, glide);
-
-            this.amp.gain.cancelScheduledValues(ctime);
-            this.amp.gain.setTargetAtTime(0.1, ctime, attack);
-            this.amp.gain.setTargetAtTime(0.0, ctime + attack, decay);
-            this.filter.frequency.cancelScheduledValues(ctime);
-            this.filter.frequency.setTargetAtTime(filterFreq, ctime, attack);
-            this.filter.frequency.setTargetAtTime(filterFreq / 2, ctime + attack, decay/2);
-        }
-    }
-    class Kick {
-        constructor() {
-            this.osc = audio.createOscillator();
-            this.osc.type = "sine";
-            this.amp = audio.createGain();
-            this.amp.gain.setValueAtTime(0, audio.currentTime);
-            this.osc.connect(this.amp);
-            this.osc.start(audio.currentTime);
-            this.out = this.amp;
-        }
-        connect(target) {
-            this.out.connect(target);
-        }
-
-        play() {
-            let ctime = audio.currentTime+0.01;
-            this.amp.gain.cancelScheduledValues(ctime);
-            this.amp.gain.setTargetAtTime(0.3, ctime, 0.002);
-            this.amp.gain.setTargetAtTime(0, ctime +0.002, 0.1);
-            this.osc.frequency.cancelScheduledValues(ctime);
-            this.osc.frequency.setTargetAtTime(440, ctime, 0.002);
-            this.osc.frequency.setTargetAtTime(55, ctime +0.002, + 0.02);
-        }
-    }
-
-    class FeedbackDelay {
-        constructor(delayTime, feedback) {
-            this.delay = audio.createDelay();
-            this.delay.delayTime.setValueAtTime(delayTime, audio.currentTime);
-            this.delayFeedbackGain = audio.createGain();
-            this.delayFeedbackGain.gain.setValueAtTime(feedback, audio.currentTime);
-            this.feedback = this.delayFeedbackGain.gain;
-            this.delay.connect(this.delayFeedbackGain);
-
-            this.in = this.delay;
-            this.out = this.delayFeedbackGain;
-            this.delayFeedbackGain.connect(this.delay);
-        }
-    }
-
-    class Panner {
-        constructor(pan) {
-            if (audio.createStereoPanner) {
-                this.panner = audio.createStereoPanner();
-                this.panner.pan.setValueAtTime(pan, audio.currentTime);
-            } else {
-                // https://stackoverflow.com/questions/52809552/how-to-pan-audio-in-ios-web-audio-implementation
-                this.panner = audio.createPanner();
-                this.panner.panningModel = "equalpower";
-                this.panner.setPosition(pan,0,1-Math.abs(pan));
-            }
-            this.in = this.panner;
-            this.out = this.panner;
-        }
-    }
 
     const bpm = 111;
     const secondsPerBeat = 60 / bpm;
@@ -134,10 +42,10 @@ function start() {
     let baseKey = rndInt(12);
 
     function createPart(baseAPitch = 110, scale, wave="sawtooth", pan = 0) {
-        const monoSynth = new MonoSynth(wave);
-        const delay = new FeedbackDelay(secondsPer16th * 3 + Math.random() * 0.02 - 0.01, 0.5);
+        const monoSynth = new MonoSynth(audio, wave);
+        const delay = new FeedbackDelay(audio, secondsPer16th * 3 + Math.random() * 0.02 - 0.01, 0.5);
 
-        let panner = new Panner(pan);
+        let panner = new Panner(audio, pan);
 
         monoSynth.connect(panner.in);
         monoSynth.connect(delay.in);
@@ -277,7 +185,7 @@ function start() {
         g.restore();
     }
 
-    const kick = new Kick();
+    const kick = new Kick(audio);
     kick.out.connect(mainOut);
 
     const left = createPart(110, SCALES.minPent, "sawtooth", -0.6);
